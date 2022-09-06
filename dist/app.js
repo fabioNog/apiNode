5a8ab46343888d4c9bd4e1582d207b384e0138cb
fabioNog <fabio.nogueira.carmo@gmail.com>
2022-09-06 16:52:37 -0300

    Create dist with npm run dev

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class App {
+    constructor() {
+        this.express = (0, express_1.default)();
+        this.database();
+        this.middlewares();
+        this.router();
+    }
+    router() {
+    }
+    database() {
+    }
+    middlewares() {
+    }
+}
+exports.default = App;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function getNome(aluno) {
-    return aluno.nome;
-}
-let aluno = {
-    nome: "Pedro",
-    idade: 18
-};
-getNome(aluno);
